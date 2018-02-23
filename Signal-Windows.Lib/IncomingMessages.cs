a65d151a6809b46170962837eb0f728fd7763753
Benedikt Constantin Radtke <benediktradtke@mailbox.tu-berlin.de>
2018-02-23 09:01:48 +0100

    save contentype

--- Signal-Windows.Lib/IncomingMessages.cs
+++ Signal-Windows.Lib/IncomingMessages.cs
@@ -435,7 +435,7 @@ namespace Signal_Windows.Lib
                         Message = message,
                         Status = (uint)SignalAttachmentStatus.Default,
                         SentFileName = pointer.FileName,
-                        ContentType = "",
+                        ContentType = receivedAttachment.getContentType(),
                         Key = pointer.Key,
                         Relay = pointer.Relay,
                         StorageId = pointer.Id,
