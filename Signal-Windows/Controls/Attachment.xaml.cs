93ad2cc44e6337d49763f1ceba8792343c727766
Benedikt Constantin Radtke <benediktradtke@mailbox.tu-berlin.de>
2018-12-05 15:13:34 +0100

    Add very basic AttachmentDetailsPage

--- Signal-Windows/Controls/Attachment.xaml.cs
+++ Signal-Windows/Controls/Attachment.xaml.cs
@@ -11,7 +11,7 @@ using Signal_Windows.Storage;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Storage;
-
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -88,6 +88,22 @@ namespace Signal_Windows.Controls
             App.Handle.StartAttachmentDownload(Model);
         }
 
+        private void AttachmentImage_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (IsDetailsPageEnabled)
+            {
+                App.CurrentSignalWindowsFrontend(ApplicationView.GetForCurrentView().Id).Locator.MainPageInstance.OpenAttachment(Model);
+            }
+        }
+
+        public bool HandleUpdate(SignalAttachment sa)
+        {
+            DataContext = sa;
+            return Model.Status != SignalAttachmentStatus.Finished && Model.Status != SignalAttachmentStatus.Failed_Permanently;
+        }
+
+        private bool IsDetailsPageEnabled => IMAGE_TYPES.Contains(Model.ContentType);
+
         private static HashSet<string> IMAGE_TYPES = new HashSet<string>()
         {
             "image/jpeg",
@@ -95,11 +111,5 @@ namespace Signal_Windows.Controls
             "image/gif",
             "image/bmp"
         };
-
-        public bool HandleUpdate(SignalAttachment sa)
-        {
-            DataContext = sa;
-            return Model.Status != SignalAttachmentStatus.Finished && Model.Status != SignalAttachmentStatus.Failed_Permanently;
-        }
     }
 }
---
+++ Signal-Windows/Views/AttachmentDetailsPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Signal_Windows.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Signal_Windows.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AttachmentDetailsPage : Page
+    {
+        public ObservableCollection<string> Attachments { get; set; } = new ObservableCollection<string>();
+
+        public AttachmentDetailsPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public string Path { get; set; }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            Utils.EnableBackButton(BackButton_Click);
+            Attachments.Clear();
+            Attachments.Add($"{ApplicationData.Current.LocalCacheFolder.Path}/Attachments/{(e.Parameter as SignalAttachment).Id}.plain");
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+            Utils.DisableBackButton(BackButton_Click);
+        }
+
+        private void BackButton_Click(object sender, BackRequestedEventArgs e)
+        {
+            Frame.GoBack();
+            e.Handled = true;
+        }
+    }
+}
